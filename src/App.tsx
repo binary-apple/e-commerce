04dfa181a00b0130ad95ca9e0e250f0436041b09
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2025-05-10 00:02:02 +0200

    refactor: remove commented lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
-// import Button from '@mui/material/Button';
 import { theme } from './theme';
 import RegistrationPage from './features/registration/RegistrationPage';
 
 export default function App() {
-  // const message: string = 'React + TypeScript + Eslint installed! 🚀';
   return (
     <>
       <ThemeProvider theme={theme}>

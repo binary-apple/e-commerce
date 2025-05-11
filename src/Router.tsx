99cda3652286865faa2331415a48f796bf15a0df
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-05-12 02:11:57 +0300

    feat: implement router in App

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,23 @@
-// TODO: implement router configuration
+import { Routes, Route } from 'react-router';
+import MainLayout from './features/main/layout/MainLayout';
+import MainPage from './features/main/MainPage.tsx';
+import CatalogPage from './features/catalog/CatalogPage.tsx';
+import RegistrationPage from './features/registration/RegistrationPage.tsx';
+import NotFoundPage from './features/notFound/NotFoundPage';
+import AuthenticationPage from './features/login/AuthenticationPage';
 
-export function Router() {
-  return <Router />;
+export default function Router() {
+  return (
+    <>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="catalog" element={<CatalogPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+        <Route path="registration" element={<RegistrationPage />} />
+        <Route path="login" element={<AuthenticationPage />} />
+      </Routes>
+    </>
+  );
 }

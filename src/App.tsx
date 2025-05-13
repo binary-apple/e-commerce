dc7f442eacdc7eadb0bb8955e68c0fd42c0364be
Alexey Yakovlev <yakovlevworkby@gmail.com>
2025-05-13 20:27:02 +0000

    Revert "feat: using router in app"
    This reverts commit 778b4fa778770e7856d17b6b76c767e27f11f50e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './theme';
-import Router from './Router';
+import RegistrationPage from './features/registration/RegistrationPage';
 
 export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Router />
+        <RegistrationPage />
       </ThemeProvider>
     </>
   );

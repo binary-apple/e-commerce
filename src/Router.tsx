29b7575ba08329d3e360117b076d5ede3a734cb4
Alexey Yakovlev <yakovlevworkby@gmail.com>
2025-05-13 21:36:49 +0000

    fix: update router to current state of app

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,22 +1,26 @@
-import { Routes, Route } from 'react-router';
-import MainLayout from './features/main/layout/MainLayout';
-import MainPage from './features/main/MainPage.tsx';
-import CatalogPage from './features/catalog/CatalogPage.tsx';
-import RegistrationPage from './features/registration/RegistrationPage.tsx';
+import { Routes, Route, Outlet } from 'react-router';
+import RegistrationPage from './features/registration/RegistrationPage';
 import NotFoundPage from './features/notFound/NotFoundPage';
-import AuthenticationPage from './features/login/AuthenticationPage';
+// Todo: remove import outlet after creating main layout
 
 export default function Router() {
   return (
     <>
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<MainPage />} />
-          <Route path="catalog" element={<CatalogPage />} />
+        <Route
+          element={
+            <>
+              <div>Layout of website</div>
+              <Outlet />
+            </>
+          }
+        >
+          <Route path="/" element={<div>Main Page</div>} />
+          <Route path="catalog" element={<div>Catalog Page</div>} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
         <Route path="registration" element={<RegistrationPage />} />
-        <Route path="login" element={<AuthenticationPage />} />
+        <Route path="login" element={<div>Log In</div>} />
       </Routes>
     </>
   );

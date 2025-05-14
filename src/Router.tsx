73f4732b93aff05e253e9c5d7bbd10542d42cace
Anna Perevedentseva <anna.pereved@gmail.com>
2025-05-14 15:44:44 +0200

    refactor: move router paths into enum

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Outlet } from 'react-router';
 import RegistrationPage from './features/registration/RegistrationPage';
 import NotFoundPage from './features/notFound/NotFoundPage';
 // Todo: remove import outlet after creating main layout
+import { Paths } from './types/paths';
 
 export default function Router() {
   return (
@@ -15,11 +16,11 @@ export default function Router() {
             </>
           }
         >
-          <Route path="/" element={<div>Main Page</div>} />
-          <Route path="catalog" element={<div>Catalog Page</div>} />
-          <Route path="registration" element={<RegistrationPage />} />
-          <Route path="login" element={<div>Log In</div>} />
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path={Paths.HOME} element={<div>Main Page</div>} />
+          <Route path={Paths.CATALOG} element={<div>Catalog Page</div>} />
+          <Route path={Paths.REGISTRATION} element={<RegistrationPage />} />
+          <Route path={Paths.AUTH} element={<div>Log In</div>} />
+          <Route path={Paths.NOT_FOUND} element={<NotFoundPage />} />
         </Route>
       </Routes>
     </>

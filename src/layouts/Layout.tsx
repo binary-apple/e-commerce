0548187f75e714c9285dd7b738d97b3ddafeb7bc
Anna Perevedentseva <anna.pereved@gmail.com>
2025-05-15 14:38:38 +0200

    refactor: remove redundant CssBaseline component from mani layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,12 +1,11 @@
 import Footer from './components/Footer/Footer';
 import { Outlet } from 'react-router';
-import { Box, CssBaseline } from '@mui/material';
+import Box from '@mui/material/Box';
 import Header from './components/Header/Header';
 
 export function Layout() {
   return (
     <>
-      <CssBaseline />
       <Header />
       <Box component="main" flexGrow={1} display="flex" justifyContent="center" alignItems="center">
         <Outlet />

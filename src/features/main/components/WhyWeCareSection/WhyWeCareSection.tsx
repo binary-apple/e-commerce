8f7a8599cf6f4bff0f41fe232e95fdeedf154e35
Anna Perevedentseva <anna.pereved@gmail.com>
2025-05-17 16:58:40 +0200

    feat: add background images

--- src/features/main/components/WhyWeCareSection/WhyWeCareSection.tsx
+++ src/features/main/components/WhyWeCareSection/WhyWeCareSection.tsx
@@ -2,18 +2,31 @@ import Box from '@mui/material/Box';
 import DetailsCard from '../DetailsCard/DetailsCard';
 import { cardsData, title } from './constants';
 import Title from '../../../../components/Title/Title';
+import classes from './whyWeCareSection.module.scss';
 
 export default function WhyWeCareSection() {
   return (
-    <Box component="section" sx={{ display: 'flex', flexDirection: 'column', gap: 2, py: 12 }}>
-      <Title title={title} />
+    <Box component="section" className={classes.section}>
       <Box
         component="div"
-        sx={{ display: 'flex', flexDirection: { md: 'row', xs: 'column' }, gap: 2 }}
+        sx={{
+          maxWidth: { lg: '1108px', md: '850px', xs: 'fit-content' },
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          margin: 'auto',
+          py: 12,
+        }}
       >
-        {cardsData.map(({ title, imgUrl, text, isBg: bgImg }, i) => (
-          <DetailsCard key={i} title={title} imgUrl={imgUrl} text={text} isBg={bgImg} />
-        ))}
+        <Title title={title} />
+        <Box
+          component="div"
+          sx={{ display: 'flex', flexDirection: { md: 'row', xs: 'column' }, gap: 2 }}
+        >
+          {cardsData.map(({ title, imgUrl, text, isBg: bgImg }, i) => (
+            <DetailsCard key={i} title={title} imgUrl={imgUrl} text={text} isBg={bgImg} />
+          ))}
+        </Box>
       </Box>
     </Box>
   );

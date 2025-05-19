46edd3f687a2d5e3afd6bfd5f46395bf2679216b
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-05-19 04:00:46 +0300

    fix: set name of test

--- src/features/notFound/components/OopsBox.test.tsx
+++ src/features/notFound/components/OopsBox.test.tsx
@@ -5,7 +5,7 @@ import OopsBox from './OopsBox';
 import { NotFoundConstants } from '../constants';
 
 describe('OopsBox', () => {
-  it('Must render ', () => {
+  it('Must render Oops box with text error end code of error.', () => {
     render(<OopsBox />);
 
     expect(screen.getByRole('heading', { name: NotFoundConstants.errorText })).toBeInTheDocument();

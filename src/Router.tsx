fac3b73dbe89023bc41c30db211e3510eb64e399
Anna Perevedentseva <anna.pereved@gmail.com>
2025-05-24 15:53:18 +0200

    feat: add catalog page

--- src/Router.tsx
+++ src/Router.tsx
@@ -10,6 +10,7 @@ import { useSelector } from 'react-redux';
 import type { RootState } from './store/store';
 import CircularProgress from '@mui/material/CircularProgress';
 import ProfilePage from './features/profile/ProfilePage';
+import Catalog from './features/catalog/Catalog';
 
 type AuthGuardProps = {
   children: ReactNode;
@@ -41,7 +42,7 @@ export default function Router() {
         <Routes>
           <Route element={<Layout />}>
             <Route path={Paths.HOME} element={<MainPage />} />
-            <Route path={Paths.CATALOG} element={<div>Catalog Page</div>} />
+            <Route path={Paths.CATALOG} element={<Catalog />} />
             <Route path={Paths.ABOUT} element={<div>About Page</div>} />
 
             <Route

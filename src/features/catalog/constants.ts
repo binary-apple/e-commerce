57dabfd99bf88199f9f94f1c9e01805a51c30106
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-05-25 22:40:55 +0300

    feat: update data with needed information

--- src/features/catalog/constants.ts
+++ src/features/catalog/constants.ts
@@ -7,13 +7,17 @@ export const products: ProductConfig[] = [
     image: 'https://binary-apple.github.io/furever-images/img-webp/rabbit-1.webp',
     description:
       'Europe’s largest animal shelter caring for around 1,300 animals daily including dogs, cats, rabbits and birds.',
+    place: 'Berlin, Germany',
+    color: 'color-average',
   },
   {
     name: 'Battersea Dogs & Cats Home',
     price: 50,
-    image: 'https://binary-apple.github.io/furever-images/img-webp/cat-1.webp',
+    image: 'https://binary-apple.github.io/furever-images/img-webp/cat-6.webp',
     description:
       'Historic animal shelter in London, caring for lost and abandoned dogs and cats, and rehoming thousands of pets.',
+    place: 'Berlin, Germany',
+    color: 'color-expensive',
   },
   {
     name: 'CottonTails Rabbit & Guinea Pig Rescue',
@@ -21,13 +25,17 @@ export const products: ProductConfig[] = [
     image: 'https://binary-apple.github.io/furever-images/img-webp/cat-2.webp',
     description:
       'UK charity dedicated to rescuing and rehoming rabbits and guinea pigs, providing care and resources for these small pets.',
+    place: 'Berlin, Germany',
+    color: 'color-cheap',
   },
   {
     name: 'A Cupulatta (La Cité des Tortues)',
     price: 100,
-    image: 'https://binary-apple.github.io/furever-images/img-webp/cat-4.webp',
+    image: 'https://binary-apple.github.io/furever-images/img-webp/cat-5.webp',
     description:
       'Tortoise sanctuary in Corsica covering 2.5 hectares and housing over 3,000 animals of 170 species.',
+    place: 'Berlin, Germany',
+    color: 'color-average',
   },
   {
     name: 'Battersea Dogs & Cats Home',
@@ -35,12 +43,16 @@ export const products: ProductConfig[] = [
     image: 'https://binary-apple.github.io/furever-images/img-webp/dog-1.webp',
     description:
       'Historic animal shelter in London, caring for lost and abandoned dogs and cats, and rehoming thousands of pets.',
+    place: 'Berlin, Germany',
+    color: 'color-cheap',
   },
   {
     name: 'CottonTails Rabbit & Guinea Pig Rescue',
     price: 25,
-    image: 'https://binary-apple.github.io/furever-images/img-webp/dog-2.webp',
+    image: 'https://binary-apple.github.io/furever-images/img-webp/dog-4.webp',
     description:
       'UK charity dedicated to rescuing and rehoming rabbits and guinea pigs, providing care and resources for these small pets.',
+    place: 'Berlin, Germany',
+    color: 'color-cheap',
   },
 ];

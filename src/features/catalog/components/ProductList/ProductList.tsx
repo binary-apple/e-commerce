73a5cc0d13a67168490c297b3c850f17d70b3bb7
Anna Perevedentseva <anna.pereved@gmail.com>
2025-05-26 17:30:31 +0200

    fix: catalog styles

--- src/features/catalog/components/ProductList/ProductList.tsx
+++ src/features/catalog/components/ProductList/ProductList.tsx
@@ -15,38 +15,26 @@ export default function ProductList() {
     return <Box>Error {JSON.stringify(error)}</Box>;
   }
   return (
-    <Box
-      component="div"
-      sx={{
-        maxWidth: { lg: '1108px', md: '850px', xs: '90%' },
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 2,
-        margin: 'auto',
-        py: { lg: 8, md: 6, xs: 4 },
-      }}
-    >
-      <Grid container spacing={3}>
-        {data?.results.map((product: Product) => {
-          const price = product.masterVariant.prices[0].value;
-          const formattedPrice = (price.centAmount / CENTS_IN_EURO).toFixed(price.fractionDigits);
-          return (
-            <Grid key={product.id} size={3}>
-              {/* TODO: replace the Box below to product card */}
-              <Box display="flex" flexDirection="column">
-                <h4>{product.name['en-GB'] || ''}</h4>
-                <div>{product.description['en-GB'] || ''}</div>
-                <div>{`${formattedPrice}€`}</div>
-                <img
-                  src={product.masterVariant.images[0].url}
-                  alt={product.masterVariant.images[0].label}
-                />
-                <div>{`Color: ${product.masterVariant.attributes.find((attribute) => attribute.name === 'color')?.value.key}`}</div>
-              </Box>
-            </Grid>
-          );
-        })}
-      </Grid>
-    </Box>
+    <Grid container spacing={3}>
+      {data?.results.map((product: Product) => {
+        const price = product.masterVariant.prices[0].value;
+        const formattedPrice = (price.centAmount / CENTS_IN_EURO).toFixed(price.fractionDigits);
+        return (
+          <Grid key={product.id} size={3}>
+            {/* TODO: replace the Box below to product card */}
+            <Box display="flex" flexDirection="column">
+              <h4>{product.name['en-GB'] || ''}</h4>
+              <div>{product.description['en-GB'] || ''}</div>
+              <div>{`${formattedPrice}€`}</div>
+              <img
+                src={product.masterVariant.images[0].url}
+                alt={product.masterVariant.images[0].label}
+              />
+              <div>{`Color: ${product.masterVariant.attributes.find((attribute) => attribute.name === 'color')?.value.key}`}</div>
+            </Box>
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 }

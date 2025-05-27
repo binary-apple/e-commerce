c29cac51770884955ba7f6ba68346f0f2ae3e4da
Anna Perevedentseva <anna.pereved@gmail.com>
2025-05-27 21:59:41 +0200

    feat: improve breadcrumps style

--- src/features/catalog/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/features/catalog/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,6 +1,7 @@
 import Box from '@mui/material/Box';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import { useCategory } from '../../../../contexts/CategoryContext';
+import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 
 export default function CustomBreadcrumbs() {
@@ -8,10 +9,15 @@ export default function CustomBreadcrumbs() {
   if (isLoading || isError) return null;
   return (
     <Box>
-      <Breadcrumbs separator="›" aria-label="breadcrumb">
-        {currentCategoryChain.map((category) => (
-          <Link>{category.categoryName}</Link>
-        ))}
+      <Breadcrumbs separator="›" aria-label="breadcrumb" sx={{ height: '26px' }}>
+        {currentCategoryChain.map((category, id) => {
+          if (id === currentCategoryChain.length - 1) {
+            return (
+              <Typography sx={{ fontFamily: 'Josefin Sans' }}>{category.categoryName}</Typography>
+            );
+          }
+          return <Link>{category.categoryName}</Link>;
+        })}
       </Breadcrumbs>
     </Box>
   );

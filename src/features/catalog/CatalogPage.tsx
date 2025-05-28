851a4996ecf568884f0ae485f0e0c4e8b7373040
Anna Perevedentseva <anna.pereved@gmail.com>
2025-05-28 18:38:41 +0200

    feat: improve catalog appearance

--- src/features/catalog/CatalogPage.tsx
+++ src/features/catalog/CatalogPage.tsx
@@ -3,6 +3,7 @@ import ProductList from './components/ProductList/ProductList';
 import CategoryList from './components/CategoryList/CategoryList';
 import { CategoryProvider } from '../../providers/CategoriesProvider';
 import CustomBreadcrumbs from './components/Breadcrumbs/Breadcrumbs';
+import Grid from '@mui/material/Grid';
 
 export default function CatalogPage() {
   return (
@@ -10,21 +11,22 @@ export default function CatalogPage() {
       <Box
         component="div"
         sx={{
+          width: '100%',
           maxWidth: { lg: '1108px', md: '850px', xs: '90%' },
           gap: 2,
           margin: 'auto',
           py: { lg: 8, md: 6, xs: 4 },
         }}
       >
-        <Box width="100%" display="flex" gap={2}>
-          <Box component="div" minWidth="30%" height="fit-content">
+        <Grid container spacing={3}>
+          <Grid size={3}>
             <CategoryList />
-          </Box>
-          <Box maxWidth="calc(70% - 16px)" display="flex" flexDirection="column" gap={1}>
+          </Grid>
+          <Grid size={9} display={'grid'} gap={2}>
             <CustomBreadcrumbs />
             <ProductList />
-          </Box>
-        </Box>
+          </Grid>
+        </Grid>
       </Box>
     </CategoryProvider>
   );

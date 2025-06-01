34ff74884277c2d49a41fe3adeab90bc7a69713e
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-06-02 00:27:14 +0300

    fix: replace unnecessary parameter of slider

--- src/features/product/components/ImageSlider/ImageSlider.tsx
+++ src/features/product/components/ImageSlider/ImageSlider.tsx
@@ -22,7 +22,7 @@ export default function ImageSlider({ product }: { product: Product }) {
       slidesPerView={1}
       autoplay={{
         delay: 3000,
-        disableOnInteraction: true,
+        pauseOnMouseEnter: true,
       }}
     >
       <SwiperSlide key="sticker">

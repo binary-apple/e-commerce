0f06a5835ac76a3599c466fc17fd749e13c110c7
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-06-02 18:32:39 +0300

    feat: implement image modal state manager

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { authApi } from '../api/authApi';
 import { authReducer } from './slices/authSlice';
 import { productsApi } from '../api/productsApi';
+import imageModalReducer from './slices/imageModalSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
     [productsApi.reducerPath]: productsApi.reducer,
+    imageModal: imageModalReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().prepend(authApi.middleware, productsApi.middleware),

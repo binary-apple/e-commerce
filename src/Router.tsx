2cdb9846695e0a678744ed801b32293948e3d179
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2025-06-09 23:02:43 +0300

    feat: add message and link

--- src/Router.tsx
+++ src/Router.tsx
@@ -12,6 +12,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 import ProfilePage from './features/profile/ProfilePage';
 import CatalogPage from './features/catalog/CatalogPage';
 import ProductPage from './features/product/ProductPage';
+import CartPage from './features/cart/CartPage';
 
 type AuthGuardProps = {
   children: ReactNode;
@@ -46,7 +47,7 @@ export default function Router() {
             <Route path={Paths.CATALOG} element={<CatalogPage />} />
             <Route path={Paths.PRODUCT} element={<ProductPage />} />
             <Route path={Paths.ABOUT} element={<div>About Page</div>} />
-            <Route path={Paths.CART} element={<div>Cart page</div>} />
+            <Route path={Paths.CART} element={<CartPage />} />
 
             <Route
               path={Paths.REGISTRATION}

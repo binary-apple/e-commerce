b65fef1c2d33344698ba88148443b1687a165071
Alexey Yakovlev <yakovlevworkby@gmail.com>
2025-06-13 21:11:50 +0000

    fix: remove default from exports

--- src/types/cartApiGuards.ts
+++ src/types/cartApiGuards.ts
@@ -1,12 +1,12 @@
 import type { Response } from './productsApi';
 
-export default function isCartListResponse(data: unknown): data is Response<Cart> {
+export function isCartListResponse(data: unknown): data is Response<Cart> {
   return (
     typeof data === 'object' && data !== null && 'results' in data && Array.isArray(data.results)
   );
 }
 
-export default function isCart(data: unknown): data is Cart {
+export function isCart(data: unknown): data is Cart {
   if (typeof data !== 'object' || data === null) return false;
   return (
     typeof data === 'object' &&

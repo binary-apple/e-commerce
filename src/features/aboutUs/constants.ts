dce5e3c95762086c443e455c65168d146fb17fe0
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-06-15 06:31:23 +0300

    feat: implement draft about us page

--- src/features/aboutUs/constants.ts
+++ src/features/aboutUs/constants.ts
@@ -1,8 +1,47 @@
 export const AboutUsConstants = {
-  title: 'About us',
-  description:
-    'Welcome to our eCommerce application! ' +
-    'This platform replicates real- world shopping experiences in a digital environment.' +
-    'The main goal of the project is to develop a comprehensive online shopping portal,' +
-    'that provides an interactive and seamless experience to users.',
+  title: 'Meet the Pack 🐾',
+  description: 'Each team member is a sticker hero, crafting every detail with love.',
+  memberButtonTitle: 'Go to GitHub',
+  buttonTitle: 'Start helping!',
+  members: [
+    {
+      name: 'Anna',
+      role: 'Project manager | Front-end developer',
+      nickname: '@binary-apple',
+      github: 'https://github.com/binary-apple',
+      photo: '/images/binary-apple.webp',
+      bio: 'Draft for self-description 😻',
+      contributions: [
+        'Complete sprint 1 tasks...',
+        'Complete sprint 1 tasks...',
+        'Complete sprint 1 tasks...',
+      ],
+    },
+    {
+      name: 'Tatiana',
+      role: 'Full-Stack Sticker Wrangler & Scrum Master',
+      nickname: '@tanykos',
+      github: 'https://github.com/tanykos',
+      photo: '/images/tanykos.webp',
+      bio: 'Draft for self-description 🐾',
+      contributions: [
+        'Complete sprint 1 tasks...',
+        'Complete sprint 1 tasks...',
+        'Complete sprint 1 tasks...',
+      ],
+    },
+    {
+      name: 'Alexey',
+      role: 'Development configuration',
+      nickname: '@montaana01',
+      github: 'https://github.com/montaana01',
+      photo: '/images/montaana01.webp',
+      bio: 'Draft for self-description 🧩',
+      contributions: [
+        'Complete sprint 1 tasks...',
+        'Complete sprint 1 tasks...',
+        'Complete sprint 1 tasks...',
+      ],
+    },
+  ],
 };

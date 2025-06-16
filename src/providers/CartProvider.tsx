12921161e32dc6cf476ec5c8ec1571c1a72d57a3
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-06-16 03:47:25 +0300

    feat: update cart provider for tests

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,6 +1,12 @@
+import { useGetAnonymousSessionQuery } from '../api/authApi';
 import { useGetMyActiveCartQuery } from '../api/cartApi';
+import { saveAuthTokenToLS } from '../hooks/useAuth';
 
 export const CartProvider = () => {
+  const anonymousSession = useGetAnonymousSessionQuery();
+  if (anonymousSession.isSuccess) {
+    saveAuthTokenToLS(anonymousSession.data.access_token, anonymousSession.data.refresh_token);
+  }
   useGetMyActiveCartQuery();
   return null;
 };

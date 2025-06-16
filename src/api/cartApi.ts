8b5562b838241b828e425960e19e4c602126dbc7
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2025-06-16 21:42:13 +0300

    feat: implement promo codes in cart

--- src/api/cartApi.ts
+++ src/api/cartApi.ts
@@ -29,7 +29,14 @@ export const cartApi = createApi({
             },
           });
         }
-        const activeCart = await fetchWithBQ('me/active-cart');
+        // const activeCart = await fetchWithBQ('me/active-cart');
+        const activeCart = await fetchWithBQ({
+          url: 'me/active-cart',
+          params: {
+            expand: 'discountCodes[*].discountCode',
+          },
+        });
+
         if (activeCart.error) return { error: activeCart.error };
         if (!isCart(activeCart.data)) {
           return {
@@ -87,6 +94,56 @@ export const cartApi = createApi({
       }),
       invalidatesTags: ['Cart'],
     }),
+    addDiscountCode: build.mutation<
+      Cart,
+      {
+        cartId: string;
+        version: number;
+        code: string;
+      }
+    >({
+      query: ({ cartId, version, code }) => ({
+        url: `me/carts/${cartId}`,
+        method: 'POST',
+        body: {
+          version,
+          actions: [
+            {
+              action: 'addDiscountCode',
+              code: code.trim().toUpperCase(),
+            },
+          ],
+        },
+      }),
+      invalidatesTags: ['Cart'],
+    }),
+
+    removeDiscountCode: build.mutation<
+      Cart,
+      {
+        cartId: string;
+        version: number;
+        discountCodeId: string;
+      }
+    >({
+      query: ({ cartId, version, discountCodeId }) => ({
+        url: `me/carts/${cartId}`,
+        method: 'POST',
+        body: {
+          version,
+          actions: [
+            {
+              action: 'removeDiscountCode',
+              discountCode: {
+                typeId: 'discount-code',
+                id: discountCodeId,
+              },
+            },
+          ],
+        },
+      }),
+      invalidatesTags: ['Cart'],
+    }),
   }),
 });
 
@@ -95,4 +152,6 @@ export const {
   useAddLineItemMutation,
   useRemoveLineItemMutation,
   useChangeLineItemQuantityMutation,
+  useAddDiscountCodeMutation,
+  useRemoveDiscountCodeMutation,
 } = cartApi;

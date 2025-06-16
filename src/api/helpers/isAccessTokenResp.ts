00e81c22638f81430ec9c7ca4ab168a2f3f67605
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-06-16 03:26:52 +0300

    fix: update guard

--- src/api/helpers/isAccessTokenResp.ts
+++ src/api/helpers/isAccessTokenResp.ts
@@ -1,5 +1,7 @@
 export function isAccessTokenResponse(
   data: unknown,
 ): data is { access_token: string; refresh_token: string } {
-  return typeof data == 'object' && data !== null && 'access_token' in data;
+  return (
+    typeof data == 'object' && data !== null && 'access_token' in data && 'refresh_token' in data
+  );
 }

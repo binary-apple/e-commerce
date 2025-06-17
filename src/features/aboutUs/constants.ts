6b475bcc6593f558783a61c7f7e68fd089e87ca7
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-06-17 04:10:24 +0300

    feat: write rss constants

--- src/features/aboutUs/constants.ts
+++ src/features/aboutUs/constants.ts
@@ -6,6 +6,10 @@ export const AboutUsConstants = {
   memberButtonTitle: 'Go to GitHub',
   buttonTitle: 'Start helping!',
   constibutionsTitle: 'Key project missions:',
+  rssDescription:
+    'RS School offers a unique learning experience as a free, community-based online education initiative. The RS School has been run by the Rolling Scopes community since 2013. Today, over 600 developer-volunteers from various countries and companies assist as mentors. We believe in important ideas that guide our mission.',
+  rssUrl: 'https://rs.school/',
+  rssButtonTitle: 'Learn more',
   members: [
     {
       name: '🐾 Anna',
